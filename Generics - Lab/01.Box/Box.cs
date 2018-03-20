a63566cce393827ef54ab3c6a1a3c54102899b46
warglaivee <warglaivegm@gmail.com>
2018-03-20 20:33:10 +0200

    up

---
+++ Generics - Lab/01.Box/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class Box<T>
+{
+    private List<T> data;
+
+    public int Count => data.Count;
+
+    protected Box()
+    {
+        this.data = new List<T>();
+    }
+
+    public void Add(T element)
+    {
+        data.Insert(0, element);
+    }
+
+    public T Remove()
+    {
+        var el = data[0];
+        data.RemoveAt(0);
+        return el;
+    }
+}
