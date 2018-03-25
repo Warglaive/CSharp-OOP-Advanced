5b97e0d19fa59f9f1911c8a66bd88380eb457245
warglaivee <warglaivegm@gmail.com>
2018-03-25 15:36:09 +0300

    up

---
+++ Iterators and Comparators - Lab/01.Library/Library.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class Library
+{
+    public List<Book> books { get; }
+
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+}
