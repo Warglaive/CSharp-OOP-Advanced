353c2a0162ccc5afb3544c4800b51a1b51f694ba
warglaivee <warglaivegm@gmail.com>
2018-03-25 19:36:40 +0300

    01 done

--- Iterators and Comparators - Lab/02.LibraryIterator/Library.cs
+++ Iterators and Comparators - Lab/02.LibraryIterator/Library.cs
@@ -10,6 +10,23 @@ public class Library : IEnumerable<Book>
         this.books = new List<Book>(books);
     }
 
+    public class LibraryIterator : IEnumerable<Book>
+    {
+        public List<Book> books { get; }
+        public int currentIndex { get; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+
     public IEnumerator<Book> GetEnumerator()
     {
         return this.books.GetEnumerator();
