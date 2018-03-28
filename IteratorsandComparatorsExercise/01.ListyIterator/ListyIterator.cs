44b89fe596e57f2edde401cff89d1c552025d1ed
warglaivee <warglaivegm@gmail.com>
2018-03-28 11:03:42 +0300

    to do

---
+++ IteratorsandComparatorsExercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ListyIterator<T>
+{
+    public List<T> Collection { get; set; }
+    public int currentIndex;
+
+
+    public ListyIterator(List<T> collection)
+    {
+        this.Collection = collection;
+    }
+    public void Move()
+    {
+        currentIndex++;
+    }
+}
