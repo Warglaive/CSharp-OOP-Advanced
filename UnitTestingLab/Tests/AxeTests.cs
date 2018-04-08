083f85478b87ca6fc0e1525af79714125034879f
warglaivee <warglaivegm@gmail.com>
2018-04-08 19:35:20 +0300

    lab

---
+++ UnitTestingLab/Tests/AxeTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private const int AxeAttack = 2;
+        private const int AxeDurability = 2;
+        private const int DummyHealth = 20;
+        private const int DummyXP = 20;
+        private Axe axe;
+        private Dummy dummy;
+
+        [Test]
+        public void Axe_DecreaseDurabilityAfterEachAttack_Istrue()
+        {
+            //Arrange
+            this.axe = new Axe(AxeAttack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth, DummyXP);
+
+            //Act
+            axe.Attack(dummy);
+            //Assert
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(1),
+                "Axe Durability dosen't change after attack.");
+        }
+        [SetUp]
+        [Test]
+        public void AttackWithBrokenAxe()
+        {
+            this.axe = new Axe(AxeAttack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth, DummyXP);
+
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            Assert.That(() => axe.Attack(dummy)
+                , Throws.InvalidOperationException
+                    .With.Message.EqualTo("Axe is broken."));
+        }
+    }
+}
---
+++ UnitTestingLab/Tests/DummyTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class DummyTests
+    {
+        [Test]
+        public void Dummy_LosesHealth_WhenAttacked()
+        {
+            var dummy = new Dummy(10, 10);
+            dummy.TakeAttack(5);
+            Assert.That(dummy.Health, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void DeadDummy_ThrowException_WhenAttacked()
+        {
+            var attacker = new Axe(20, 20);
+            var dummy = new Dummy(10, 10);
+            attacker.Attack(dummy);
+            Assert.IsTrue(dummy.IsDead(), "Can't Attack dead dummy!");
+        }
+
+        [Test]
+        public void DeadDummy_CanGive_Experience()
+        {
+            var attacker = new Axe(20, 20);
+            var dummy = new Dummy(1, 10);
+            attacker.Attack(dummy);
+            Assert.IsTrue(dummy.IsDead());
+            dummy.GiveExperience();
+        }
+
+        [Test]
+        public void AliveDummy_CantGive_Experience()
+        {
+            var attacker = new Axe(9, 20);
+            var dummy = new Dummy(10, 10);
+            attacker.Attack(dummy);
+            Assert.IsFalse(dummy.IsDead(),
+                "alive dummy can't give xp");
+        }
+    }
+}
