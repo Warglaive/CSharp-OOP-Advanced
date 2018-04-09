e7375025042ab7d5fc2d62119ffdeca0054a96c8
warglaivee <warglaivegm@gmail.com>
2018-04-09 21:18:58 +0300

    up

---
+++ UnitTestingExercises/01.Database/Database.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+public class Database
+{
+    public int[] IntegersDB;
+    public const int arrayExactLength = 16;
+    private int index;
+
+    public Database(int[] integers)
+    {
+        if (integers.Length != arrayExactLength)
+        {
+            throw new InvalidOperationException();
+        }
+        this.IntegersDB = integers;
+        this.index = 0;
+    }
+
+    public void Add(int integer)
+    {
+        if (this.IntegersDB.Length == 16)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var emptyCellIndex = this.IntegersDB
+            .FirstOrDefault(e => e.Equals(0));
+
+        if (emptyCellIndex == 0)
+        {
+            this.IntegersDB[emptyCellIndex] = integer;
+        }
+    }
+
+    public void Remove()
+    {
+        if (this.IntegersDB.Length == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        Array.Resize(ref this.IntegersDB, this.IntegersDB.Length - 1);
+        if (this.IntegersDB.Length == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        this.IntegersDB[this.IntegersDB.Length - 1] = 0;
+    }
+
+    public int[] Fetch()
+    {
+        return this.IntegersDB;
+    }
+}
---
+++ UnitTestingExercises/DatabaseTests.cs/DatabaseTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+namespace DatabaseTests.cs
+{
+    public class DatabaseTests
+    {
+        private readonly int[] InvalidSizeArray = new int[20];
+        private readonly int[] ValidSizeArray = new int[16];
+        private readonly int[] EmptyArray = new int[0];
+        private int arrayExactLength = 16;
+
+        [Test]
+        public void ExactLength_MustBe_16Integers()
+        {
+            //Test length
+            var db = new Database(ValidSizeArray);
+            Assert.That(() => db.IntegersDB.Length.Equals(arrayExactLength));
+        }
+
+        [Test]
+        public void ThrowException_WhenMoreThan_ArrayExactLength()
+        {
+            //check if exception is thrown when > 16 ints
+
+            Assert.That(() => new Database(InvalidSizeArray)
+                , Throws.InvalidOperationException);
+        }
+
+        //[Test]
+        //public void AddElement_AtTheNextFreeCell_Only()
+        //{
+        //    var numbers = new int[15] { 1, 0, 3, 2, 1, 3, 4, 5, 2, 3, 4, 5, 8, 9, 1 };
+        //    var db = new Database(numbers);
+        //    db.Add(77);
+        //}
+
+        [Test]
+        public void ThrowException_WhenAddMoreElementsThan_ArrayCapacity()
+        {
+            Assert.That(() => new Database(ValidSizeArray)
+                .Add(1), Throws.InvalidOperationException);
+        }
+
+        //[Test]
+        //public void Remove_RemovingLastElement_FromArray()
+        //{
+        //    var emptyDatabase = new Database(ValidSizeArray);
+        //    //remove 16 elements
+        //    for (int i = 0; i < ValidSizeArray.Length; i++)
+        //    {
+        //        emptyDatabase.Remove();
+        //    }
+        //    //try to remove element from empty db;
+        //    Assert.That(() => emptyDatabase.Remove(),
+        //        Throws.InvalidOperationException);
+        //}
+
+        [Test]
+        public void Remove_Element_FromEmptyDB()
+        {
+            var emptyDatabase = new Database(ValidSizeArray);
+            //remove 16 elements
+            for (int i = 0; i < ValidSizeArray.Length; i++)
+            {
+                emptyDatabase.Remove();
+            }
+            //try to remove element from empty db;
+            //emptyDatabase.Remove();
+            Assert.That(() => emptyDatabase.Remove(),
+                Throws.InvalidOperationException);
+        }
+    }
+}
