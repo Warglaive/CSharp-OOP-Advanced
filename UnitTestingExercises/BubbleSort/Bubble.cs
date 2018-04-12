d1c11ce51c5b3f6b76e1bf791a004d6e6a144e95
warglaivee <warglaivegm@gmail.com>
2018-04-12 22:46:07 +0300

    Bubble

---
+++ UnitTestingExercises/BubbleSort/Bubble.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    public class Bubble
+    {
+        public List<int> integersToSort;
+        public Bubble(List<int> integers)
+        {
+            this.integersToSort = new List<int>(integers);
+        }
+
+        public void Sort()
+        {
+            var length = this.integersToSort.Count;
+
+            int temp = this.integersToSort[0];
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (this.integersToSort[i] > this.integersToSort[j])
+                    {
+                        temp = this.integersToSort[i];
+                        this.integersToSort[i] = this.integersToSort[j];
+                        this.integersToSort[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestingExercises/BubbleSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var unSortedInts = new List<int>();
+            for (int i = 9; i > 0; i--)
+            {
+                unSortedInts.Add(i);
+            }
+            var bubble = new Bubble(unSortedInts);
+            bubble.Sort();
+        }
+    }
+}
