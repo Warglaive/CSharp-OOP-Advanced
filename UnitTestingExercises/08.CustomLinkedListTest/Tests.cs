ab5e9545e3078371f075f567325ba09e8dd9a4a4
warglaivee <warglaivegm@gmail.com>
2018-04-14 18:29:40 +0300

    08.CustomLinkedList
    wtf task

---
+++ UnitTestingExercises/08.CustomLinkedListTest/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using CustomLinkedList;
+using NUnit.Framework;
+
+namespace _08.CustomLinkedListTest
+{
+    public class Tests
+    {
+        [Test]
+        public void Constructor_CreateObjectValue_Test()
+        {
+            var dynamicList = new LinkedListNode<int>(1);
+            Assert.IsTrue(dynamicList.Value == 1);
+        }
+
+        [Test]
+        public void CountFieldIntReturnTest()
+        {
+            var dynamic = new DynamicList<int>();
+            var countDefaultValue = 0;
+            Assert.AreEqual(countDefaultValue, dynamic.Count);
+        }
+
+        [Test]
+        public void DynamicListInvalidIndexExceptionTest()
+        {
+            var dynamicList = new DynamicList<int>();
+
+            Assert.That(() => dynamicList[1],
+                Throws.Exception);
+        }
+        //Add Tests
+        [Test]
+        public void DynamicListAddTest()
+        {
+            var dynamicList = new DynamicList<int>();
+            dynamicList.Add(0);
+            var expectedCount = dynamicList.Count;
+            Assert.AreEqual(dynamicList.Count, expectedCount);
+        }
+    }
+}
